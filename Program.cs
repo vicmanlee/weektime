8a5d3e708c52939b2c193c1f2b87724f26626952
李凯 <likai_ms@outlook.com>
2017-10-23 00:56:50 +0800

    update

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ namespace weektime
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseIISIntegration()
                 .Build();
     }
 }
