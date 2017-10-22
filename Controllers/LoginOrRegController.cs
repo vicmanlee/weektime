2b0ba1fc4b17b8b425523634d9b4d21fa1190f27
李凯 <likai_ms@outlook.com>
2017-10-23 00:12:39 +0800

    update

---
+++ Controllers/LoginOrRegController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using weektime.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace weektime.Controllers
+{
+    public class LoginOrRegController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            ViewData["Message"] = "Your application description page.";
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
